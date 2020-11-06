a09fc8e9c9ff03d83e87779c4402fd6c65b067a3
n-ndini7 <hp@RJs-PC>
2020-11-06 14:11:28 +0530

    [nandini] added selectionPP4 - min and max based on arithmetic opertaions

--- SelectionPP.js
+++ SelectionPP.js
@@ -1,22 +1,23 @@
-//PP3 - print unit , tens or hundred\
-const UNIT =1;
-const TEN =2;
-const HUNDRED =3;
-let num = Math.ceil(Math.random()*1000);
-console.log("Number is: "+num);
-let choice = Math.floor(Math.random()*10)%3+1;
-if(choice==UNIT)
-{
-        console.log("UNIT: ");
-        console.log(num%10);
-}
-else if(choice==TEN)
-{
-        console.log("TEN: ");
-        console.log(Math.floor(num/10)%10);
-}
-else if(choice==HUNDRED)
-{
-        console.log("HUNDRED: ");
-        console.log(Math.floor(num/100)%100);
-}
+//PP4 - print max and min based on operations
+let a = Math.floor(Math.random()*100);
+let b = Math.floor(Math.random()*100);
+let c = Math.floor(Math.random()*100);
+console.log("a: "+a);
+console.log("b: "+b);
+console.log("c: "+c);
+let op1 = a+b*c;
+console.log("a+b*c : "+op1);
+let op2 = a%b+c;
+console.log("a%b+c : "+op2);
+let op3 = c+a/b;
+console.log("c+a/b : "+op3);
+let op4 = a*b+c;
+console.log("a*b+c : "+op4);
+let max = op1;
+if(op2>max)
+    max = op2;
+if(op3>max)
+    max = op3;
+if(op4>max)
+    max = op4;
+console.log("Max: "+max);
