8cd8cff2babe565680f2f70a2a62a58ef1df3e3f
n-ndini7 <hp@RJs-PC>
2020-11-06 14:43:32 +0530

    [nandini] added selection if else PP2 - check for a date in a arnge

--- SelectionIfelsePP.js
+++ SelectionIfelsePP.js
@@ -1,33 +1,37 @@
-//PP1 - max and min among five 3 digit random numbers
-let digit1 = Math.floor(Math.random()*1000);
-let digit2 = Math.floor(Math.random()*1000);
-let digit3 = Math.floor(Math.random()*1000);
-let digit4 = Math.floor(Math.random()*1000);
-let digit5 = Math.floor(Math.random()*1000);
-console.log("Number 1: "+digit1);
-console.log("Number 2: "+digit2);
-console.log("Number 3: "+digit3);
-console.log("Number 4: "+digit4);
-console.log("Number 5: "+digit5);
-let max = digit1;
-let min = digit1;
-//max:
-if(digit2>max)
-    max = digit2;
-if(digit3>max)
-    max = digit3;
-if(digit4>max)
-    max = digit4;
-if(digit5>max)
-    max = digit5;
-//min:
-if(digit2<min)
-    min = digit2;
-if(digit3<min)
-    min = digit3;
-if(digit4<min)
-    min = digit4;
-if(digit5<min)
-    min = digit5;
-console.log("MAX: "+max);
-console.log("MIN: "+min);
+//PP2 - check if the given date is in range 
+let month = Math.floor(((Math.random()*10)%6)+1);
+console.log("Month: "+month);
+let day = Math.floor(((Math.random()*100)%31)+1);
+console.log("Day: "+day);
+if((month>=3 && month<=6) && (day>=1 && day<=31) ){
+    if(month==3 || month ==5){
+        if(month==3){
+            if(day<20){
+                console.log("False");
+            }
+            if(day>=20 && day<=31)
+            {
+                console.log("True");
+            }
+        }
+        if(month==5 && (day>=1 && day<=31))
+       { console.log("True");
+    }
+}
+    if(month==4 || month==6){
+        if(month==4 &&(day>=1 && day<=30)){
+        console.log("True");
+        }
+        if(month==6){
+            if(day>20)
+         {   console.log("False");
+        } 
+         if(day<20 && day>=1)
+         {   console.log("True");
+        }
+    }
+    }
+}else 
+{
+    console.log("False");
+}
