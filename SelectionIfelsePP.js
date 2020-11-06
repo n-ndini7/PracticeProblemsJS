6db56fbf77a4ee7fbb9c07c36c3c54d4da0af1b1
n-ndini7 <hp@RJs-PC>
2020-11-06 14:49:01 +0530

    [nandini] added selection if else PP3 - check for leap year

--- SelectionIfelsePP.js
+++ SelectionIfelsePP.js
@@ -1,37 +1,15 @@
-//PP2 - check if the given date is in range 
-let month = Math.floor(((Math.random()*10)%6)+1);
-console.log("Month: "+month);
-let day = Math.floor(((Math.random()*100)%31)+1);
-console.log("Day: "+day);
-if((month>=3 && month<=6) && (day>=1 && day<=31) ){
-    if(month==3 || month ==5){
-        if(month==3){
-            if(day<20){
-                console.log("False");
-            }
-            if(day>=20 && day<=31)
-            {
-                console.log("True");
-            }
-        }
-        if(month==5 && (day>=1 && day<=31))
-       { console.log("True");
+//PP3 - check if the given year is leap year
+let year = Math.floor(Math.random()*10000);
+console.log("Year : "+ year);
+if(year%4==0){
+    if(year%100==0){
+        if(year%400==0)
+            console.log("Leap Year");
+        else
+            console.log("Not a leap year");
     }
+    else 
+        console.log("Leap Year");
 }
-    if(month==4 || month==6){
-        if(month==4 &&(day>=1 && day<=30)){
-        console.log("True");
-        }
-        if(month==6){
-            if(day>20)
-         {   console.log("False");
-        } 
-         if(day<20 && day>=1)
-         {   console.log("True");
-        }
-    }
-    }
-}else 
-{
-    console.log("False");
-}
+else 
+    console.log("Not a leap year");
