14ddd1387a86f6b5ef665dc57b59fff1441df50d
n-ndini7 <hp@RJs-PC>
2020-11-06 15:37:21 +0530

    [nandini] added for loop PP3 - factorial of a number

--- ForLoopPP.js
+++ ForLoopPP.js
@@ -1,21 +1,8 @@
-//PP4
-let start = Math.ceil(Math.random()*10); 
-let end = Math.ceil(Math.random()*100);
-console.log("Start Range: "+start);
-console.log("End Range: "+end);
-if(end>start){
-        console.log("Prime numbers are: ");
-        for(let i=start;i<=end;i++){
-            for(let j=2;j<=i;j++){
-                if(j==i){
-                    console.log(i);
-                }
-                if(i%j==0)
-                    break;   
-            }
-    }
+//PP5
+let num = Math.ceil(Math.random()*10); 
+console.log("Number = "+num);
+let factorial=1;
+for(let i=1;i<=num;i++){
+    factorial = factorial*i;
 }
-else 
-{
-    console.log("Invalid range!");
-}
+console.log("Factorial: "+factorial);
